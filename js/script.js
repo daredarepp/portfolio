1b47c17656c780b9a4324daf64399642bdc4e4dd
Darko Todoroski <daredarepp@yahoo.com>
2020-11-13 18:30:44 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+window.addEventListener("load", function loadHandler() {
+  // Toggle dark theme
+  let btn = document.querySelector("button");
+  btn.addEventListener("click", () => {
+    document.documentElement.hasAttribute("theme")
+      ? document.documentElement.removeAttribute("theme")
+      : document.documentElement.setAttribute("theme", "dark");
+  });
+
+  // Activate slider
+  const slider = new KeenSlider("#slider", {
+    slidesPerView: 6,
+    loop: true,
+    spacing: 20,
+    // centered: true,
+    created: function (instance) {
+      document
+        .getElementById("arrow-left")
+        .addEventListener("click", function () {
+          instance.prev()
+        })
+  
+      document
+        .getElementById("arrow-right")
+        .addEventListener("click", function () {
+          instance.next()
+        })
+      var dots_wrapper = document.getElementById("dots")
+      var slides = document.querySelectorAll(".keen-slider__slide")
+      slides.forEach(function (t, idx) {
+        var dot = document.createElement("button")
+        dot.classList.add("dot")
+        dots_wrapper.appendChild(dot)
+        dot.addEventListener("click", function () {
+          instance.moveToSlide(idx)
+        })
+      })
+      updateClasses(instance)
+    },
+    slideChanged(instance) {
+      console.log('slide changed, instance: ', instance);
+      updateClasses(instance)
+    },
+    afterChange(instance) {
+      console.log('after change, instance: ', instance);
+    },
+    dragEnd(instance) {
+      console.log('drag end, instance: ', instance);
+    }
+  })
+  
+  function updateClasses(instance) {
+    var slide = instance.details().relativeSlide
+    var arrowLeft = document.getElementById("arrow-left")
+    var arrowRight = document.getElementById("arrow-right")
+    slide === 0
+      ? arrowLeft.classList.add("arrow--disabled")
+      : arrowLeft.classList.remove("arrow--disabled")
+    slide === instance.details().size - 1
+      ? arrowRight.classList.add("arrow--disabled")
+      : arrowRight.classList.remove("arrow--disabled")
+  
+    var dots = document.querySelectorAll(".dot")
+    dots.forEach(function (dot, idx) {
+      idx === slide
+        ? dot.classList.add("dot--active")
+        : dot.classList.remove("dot--active")
+    })
+  }
+});
